6f0b1019b03d76663548e2f26f572f0dcfb157ba
lucianocampos <57565555+lucianocampos@users.noreply.github.com>
2019-11-09 15:11:19 -0300

    Update carrera.c

--- TF/Juegos de luces/guia/carrera.c
+++ TF/Juegos de luces/guia/carrera.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include "easypio.h"
+#include <wiringPi.h>
 #include "kbhit.h"
 
 #define N 8		// numero de gpio a usar
-#define v 0.4*1000000 	// retardo en seg*cte usleep
+#define v 0.4*1000 	// retardo en seg*cte usleep
 
 void dec_bin(int dec, int *bin);
 void set(int const*gpio, int *valor);
 
 int main (){
 	
-	pioInit();
+	wiringPiSetup();
 	
 	int i;
-	int const gpio[N]={26,21,20,16,12,25,24,23};
+	int const gpio[N]= {25,29,28,27,26,6,5,4};	//lista de GPIO disponibles en la placa(se trunca con N)
 	int valor_gpio[N]={0};
 	int tabla[]={1,2,4,8,16,17,18,36,40,80,96,128,0};
 				
@@ -25,10 +25,9 @@ int main (){
 		for(i=0; i<14; i++){
 			dec_bin(tabla[i], valor_gpio);
 			set( gpio, valor_gpio);
-			if(tabla[i]<=4)	usleep(v);
-			else	usleep(v/2);
+			if(tabla[i]<=4)	delay(v);
+			else	delay(v/2);
 		}
-		
 	};
 	
 	for(i=0; i<N; i++)	digitalWrite(gpio[i],0);
@@ -41,8 +40,7 @@ void dec_bin(int dec, int *bin ){
 	int i;
 
 	if(!dec){
-		*bin=0;
-		//usleep(v/2);
+		for (i=0; i<8;i++) *(bin+i)=0;
 		return;
 	}
 
@@ -54,7 +52,6 @@ void dec_bin(int dec, int *bin ){
 }
 		 
 void set(int const *gpio, int *valor){
-	int i;
-	for (i=0; i<N; i++) digitalWrite( *(gpio+i), *(valor+i));
+	for (int i=0; i<N; i++) digitalWrite( *(gpio+i), *(valor+i));
 }
 	
